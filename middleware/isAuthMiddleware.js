c5af2c75151c98881828c206876a30e6fc5cea14
ElMartino24 <86724612+ElMartino24@users.noreply.github.com>
2023-08-06 00:08:49 +0200

    Add files via upload

--- middleware/isAuthMiddleware.js
+++ middleware/isAuthMiddleware.js
@@ -1,21 +1,21 @@
 import jwt from "jsonwebtoken";
 
 export const isAuth = (req, res, next) => {
-  // const JWT_SECRET = 5345345631;
+   const JWT_SECRET = 5345345631;
 
   if (req.method === "OPTIONS") {
     return next();
   }
 
   try {
-    // const token = req.cookies.token;
+    const token = req.cookies.token;
 
-    // if (!token) {
-    //   return res.status(401).json("Unauthorized");
-    // }
+    if (!token) {
+      return res.status(401).json("Kein Token");
+    }
 
-    // const verified = jwt.verify(token, JWT_SECRET);
-    // req.user = verified.userId;
+    const verified = jwt.verify(token, JWT_SECRET);
+    req.user = verified.userId;
 
     next();
   } catch (err) {
