06e60a483f945620bfebf1dbcaaa14ebe0bf6629
ElMartino24 <86724612+ElMartino24@users.noreply.github.com>
2023-08-06 11:22:49 +0200

    Add files via upload

--- middleware/isAuthMiddleware.js
+++ middleware/isAuthMiddleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const isAuth = (req, res, next) => {
-   const JWT_SECRET = 5345345631;
+   const JWT_SECRET = process.env.jwt;
 
   if (req.method === "OPTIONS") {
     return next();
